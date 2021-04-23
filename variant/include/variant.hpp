9f0247a4fb0d7b4980df264a14b190d802e49a2d
Дмитрий Барышников <mymailis106@gmail.com>
2021-04-23 11:39:31 +0300

    variant

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -11,54 +11,89 @@
 #include <memory>
 #include <variant>
 #include <vector>
+#include <type_traits>
 
 #include <boost/variant.hpp>
 
-//{ number
 struct number
-{
-
+    : public std::variant<
+    int,
+    float
+    > {
+    using variant::variant;
 };
-//}
 
-//{ array
 struct array
-{
-
+    : public std::vector<
+    number
+    > {
+    using vector::vector;
 };
-//}
 
-//{ recursive_array
 struct recursive_array
-{
-
+    : public std::vector<
+    std::variant<
+    number,
+    std::shared_ptr<recursive_array>
+    >
+    > {
+    using vector::vector;
 };
-//}
 
-//{ recursive_array2
 struct recursive_array2
-{
-
+    : public std::vector<
+    std::variant<
+    number,
+    boost::recursive_wrapper<recursive_array2>
+    >
+    > {
+    using vector::vector;
 };
-//}
 
-//{ variant_decorator
-template<???>
+template<typename... variant_types_pack>
 struct variant_decorator
-{
+    : public std::variant<variant_types_pack...> {
+    using std::variant<variant_types_pack...>::variant;
 
-    ?? as()
-    {
+    template<typename T>
+    decltype(auto) as() {
+        if constexpr (std::disjunction_v<
+            std::is_same<
+            boost::recursive_wrapper<T>, variant_types_pack
+            >...
+        >) {
+            return std::get<boost::recursive_wrapper<T>>(*this).get();
+        }
+        else {
+            return std::get<T>(*this);
+        }
+    }
 
+    template<typename T>
+    decltype(auto) as() const {
+        if constexpr (std::disjunction_v<
+            std::is_same<
+            boost::recursive_wrapper<T>, variant_types_pack
+            >...
+        >) {
+            return std::get<boost::recursive_wrapper<T>>(*this).get();
+        }
+        else {
+            return std::get<T>(*this);
+        }
     }
 };
-//}
 
-//{ recursive_map
-struct recursive_map
-{
-
+struct recursive_map : public std::map<
+    std::string,
+    variant_decorator<
+    int,
+    std::string,
+    boost::recursive_wrapper<recursive_map>,
+    bool
+    >
+> {
+    using map::map;
 };
-//}
 
 #endif // __VARIANT_HPP__
